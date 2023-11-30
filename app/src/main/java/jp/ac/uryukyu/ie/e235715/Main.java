b3b5f64ec2769e511df6db43b889e08aa646026f
Yuma Maeda <e235715@ie.u-ryukyu.ac.jp>
2023-11-30 17:00:32 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235715/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235715/Main.java
@@ -2,9 +2,12 @@ package jp.ac.uryukyu.ie.e235715;
 
 public class Main {
     public static void main(String[] args){
-
-                String str = null;
-        System.out.println(str.length());
-        
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException i){
+            System.out.println("NullPointExeption");
+            System.out.println(i.getMessage());
+        }
     }
 }
