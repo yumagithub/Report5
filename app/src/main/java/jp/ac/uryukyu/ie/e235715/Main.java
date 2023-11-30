785419ff43939a528b0bd08fa0eecee6f54b8379
Yuma Maeda <e235715@ie.u-ryukyu.ac.jp>
2023-11-30 15:48:35 +0900

    1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235715/Main.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e235715;
+
+public class Main {
+    public static void main(String[] args){
+
+                String str = null;
+        System.out.println(str.length());
+        
+    }
+}
